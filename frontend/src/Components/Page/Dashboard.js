6beff5f437e29c48042733c83d64195ea4ddd6ff
fitrianinasir <fitrianinasir8@gmail.com>
2022-09-19 21:16:30 +0700

    dashboard base added

--- frontend/src/Components/Page/Dashboard.js
+++ frontend/src/Components/Page/Dashboard.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import WebPageView from "./WebPageView";
+import MobilePageView from "./MobilePageView";
 
 function Dashboard(props) {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width <= 768;
+
   return (
     <div>
-      <h1>DASHBOARD</h1>
+      {isMobile? <MobilePageView/>: <WebPageView/>}
     </div>
   );
 }
